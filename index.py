24e180e55cd6dcad8e550e269f42d329acbff162
Alan Tirado <atirado0617@gmail.com>
2020-10-08 21:16:46 -0700

    test set to zero then 180

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import time
 from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels=16)
@@ -5,3 +6,9 @@ kit = ServoKit(channels=16)
 kit.servo[0].angle = 0
 kit.servo[1].angle = 0
 kit.servo[2].angle = 0
+
+time.sleep(2)
+
+kit.servo[0].angle = 180
+kit.servo[1].angle = 180
+kit.servo[2].angle = 180
