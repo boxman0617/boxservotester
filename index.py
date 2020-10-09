f22e2e0ddd28d824b23e012fcf75b1a6bec26dea
Alan Tirado <atirado0617@gmail.com>
2020-10-08 21:47:09 -0700

    testing 900-2100 range and actuation_range of 60

--- index.py
+++ index.py
@@ -9,6 +9,7 @@ servos = [0, 1, 2]
 def setup():
     for s in servos:
         kit.servo[s].set_pulse_width_range(900, 2100)
+        kit.servo[s].actuation_range = 160
 
 
 def set_all_to_angle(angle):
@@ -22,8 +23,8 @@ set_all_to_angle(0)
 
 time.sleep(2)
 
-set_all_to_angle(180)
+set_all_to_angle(160)
 
 time.sleep(2)
 
-set_all_to_angle(0)
+set_all_to_angle(80)
