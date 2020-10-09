878bf14e884ed91e80c9e5d62b711388568caaad
Alan Tirado <atirado0617@gmail.com>
2020-10-08 21:30:54 -0700

    testing pulse width range

--- index.py
+++ index.py
@@ -3,18 +3,27 @@ from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels=16)
 
-kit.servo[0].angle = 0
-kit.servo[1].angle = 0
-kit.servo[2].angle = 0
+servos = [0, 1, 2]
+
+
+def setup():
+    for s in servos:
+        kit.servo[s].set_pulse_width_range(500, 2400)
+
+
+def set_all_to_angle(angle):
+    for s in servos:
+        kit.servo[s].angle = angle
+
+
+setup()
+
+set_all_to_angle(0)
 
 time.sleep(2)
 
-kit.servo[0].angle = 180
-kit.servo[1].angle = 180
-kit.servo[2].angle = 180
+set_all_to_angle(180)
 
 time.sleep(2)
 
-kit.servo[0].angle = 0
-kit.servo[1].angle = 0
-kit.servo[2].angle = 0
+set_all_to_angle(0)
