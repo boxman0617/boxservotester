be9bd43d64077ac4bdb7d908b839d89ce5ab2330
Alan Tirado <atirado0617@gmail.com>
2020-10-08 21:19:04 -0700

    test set to zero then 180 then zero

--- index.py
+++ index.py
@@ -12,3 +12,9 @@ time.sleep(2)
 kit.servo[0].angle = 180
 kit.servo[1].angle = 180
 kit.servo[2].angle = 180
+
+time.sleep(2)
+
+kit.servo[0].angle = 0
+kit.servo[1].angle = 0
+kit.servo[2].angle = 0
